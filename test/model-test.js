67d31303f8ee4cceb0bac71d950b06b1dab27933
Steve Kobes <steve@kobes.ca>
2024-05-07 23:39:49 -0400

    Use Node's test runner.

--- test/model-test.js
+++ test/model-test.js
@@ -1,20 +1,17 @@
 import { Board } from "../dist/model.js";
 import assert from "assert/strict";
+import test from "node:test";
 
 // TODO: more tests
 
-describe("Board", function () {
-  describe("#toggleMark()", function () {
-    it("should toggle state", function () {
-      const b = new Board();
-      const p = {x: 0, y: 0};
-      assert.equal(false, b.getStateAt(p));
+test("toggle state", t => {
+  const b = new Board();
+  const p = {x: 0, y: 0};
+  assert.equal(false, b.getStateAt(p));
 
-      b.toggleMark();
-      assert.equal(true, b.getStateAt(p));
+  b.toggleMark();
+  assert.equal(true, b.getStateAt(p));
 
-      b.toggleMark();
-      assert.equal(false, b.getStateAt(p));
-    });
-  });
+  b.toggleMark();
+  assert.equal(false, b.getStateAt(p));
 });
